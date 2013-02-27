36c2c4192d3a9e3640eb1980c800b2d43147ea4e
Ultimate Ascension <info@eternyum.ro>
2013-02-27 15:19:01 +0200

    Quest
    Some Quest

---
+++ sql/Ultimate Development/Quest - Drake Hunt.sql
@@ -0,0 +1,9 @@
+-- Assign
+UPDATE `creature_template` SET `AIName`='SmartAI', `ScriptName`='' WHERE  `entry`=22181 LIMIT 1;
+
+
+-- Scriptul
+
+DELETE FROM `smart_scripts` WHERE `entryorguid`=22181;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
+(22181, 0, 0, 0, 8, 0, 100, 0, 40856, 0, 0, 0, 33, 23343, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 'On Spell Hit - Call Killed Monster');
